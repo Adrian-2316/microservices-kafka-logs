2a19603f9738962d17cbb8ed2365a19d53922e04
Daniel Mesa <damesamarin@gmail.com>
2022-06-28 17:54:18 +0200

    docker services kibana and elastic conection with elastic search

--- consumer/src/test/java/com/kafka/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/kafka/consumer/ConsumerApplicationTests.java
@@ -1,13 +1,9 @@
 package com.kafka.consumer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- producer/src/test/java/com/kafka/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/kafka/producer/ProducerApplicationTests.java
@@ -1,13 +1,9 @@
 package com.kafka.producer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProducerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
